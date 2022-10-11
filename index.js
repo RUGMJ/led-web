30ee0d24d6c49a4d5f7c4c0684d90dbadc81581e
RUGMJ7443 <matthewrhysjones@icloud.com>
2022-10-11 16:49:29 +0100

    🎉 Started Work on the server

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ const app = express();
 app.use(express.static('./public'));
 
 app.listen(3000);
+
+let color = '#000000';
+
+app.get('/api/', (req, res) => {
+	res.json(color);
+});
+
+app.post('/api/:hex', (req, res) => {
+	color = req.params.hex;
+
+	updateLeds(color);
+
+	res.json(req.params.hex);
+});
+
+function updateLeds(hex) {
+	// TODO
+}
